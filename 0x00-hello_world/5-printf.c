48bdb9b3e88c7d5c0c86bf366e30930059e3877f
OukaHarry <harryodhiambo17@gmail.com>
2021-06-21 12:10:43 -0700

    add

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ *File: 5-printf.c
+ *
+ *Written by Oukaharry
+ *
+ */
+#include <stdio.h>
+/**
+ *main - prints "with proper gramar, but the outcome is a piece of art,"
+ *
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
