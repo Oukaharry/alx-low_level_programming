eac7aa8df3196bd8df608f6fe2ed2684330296cb
OukaHarry <harryodhiambo17@gmail.com>
2021-06-22 09:16:16 -0700

    add files

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -8,8 +8,8 @@
 int main(void)
 {
     int r;
-
-    print_last_digit(98);
+	
+    r = print_last_digit(98);
     print_last_digit(0);
     r = print_last_digit(-1024);
     _putchar('0' + r);
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,3 +1,5 @@
+#include "holberton.h"
+
 /**
  * print_last_digit  - Short description, single line
  * @n: contians value to be compared
@@ -6,6 +8,12 @@
 
 int print_last_digit(int n)
 {
+	if(n < 0)
+	{
+		n = (n * -1);
+	}
 	n = (n % 10);
+
+	_putchar('0' + n);
 	return (n);
 }
