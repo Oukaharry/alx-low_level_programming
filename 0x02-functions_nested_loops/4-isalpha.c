43ddbc0783d0163ef011fc646a66e20fd5e3b05d
OukaHarry <harryodhiambo17@gmail.com>
2021-06-22 07:12:05 -0700

    add file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _isalpha  - Short description, single line
+ * @c: contians value to be compared
+ * Return: Always returns zero
+ */
+
+int _isalpha(int c)
+{
+	int isalpha = 0;
+	char comp = c;
+
+	if ((comp >= 'A' && comp <= 'Z') || (comp >= 'a' && comp <= 'z'))
+	{
+		isalpha = 1;
+	}
+	return (isalpha);
+}
