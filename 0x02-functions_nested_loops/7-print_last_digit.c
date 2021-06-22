42370c5c7b98ea40345944202f301b96e54db773
OukaHarry <harryodhiambo17@gmail.com>
2021-06-22 08:32:01 -0700

    add files

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+/**
+ * print_last_digit  - Short description, single line
+ * @n: contians value to be compared
+ * Return: the value of the last digit
+ */
+
+int print_last_digit(int n)
+{
+	n = (n % 10);
+	return (n);
+}
