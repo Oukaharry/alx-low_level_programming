626ea08e8f9ee64bc8819e4937704bb4630789f6
OukaHarry <harryodhiambo17@gmail.com>
2021-06-21 22:59:10 -0700

    add file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 is success
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'e' && ch != 'q')
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
