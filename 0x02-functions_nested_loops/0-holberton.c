9c17fb69ca5754c9e4a7d8f530013000893ccfe3
OukaHarry <harryodhiambo17@gmail.com>
2021-06-22 02:50:52 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * main - Prints alphabet in lowercase,
+ * and then in uppercase.
+ *
+ *Return: Always 0.
+ */
+
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains file for the Functions, Nested Loops project
