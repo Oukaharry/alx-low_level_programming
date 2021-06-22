ced8b9efddb41431cbdef66938ed05fa419b4199
OukaHarry <harryodhiambo17@gmail.com>
2021-06-22 09:59:34 -0700

    final fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,13 @@
 
 int print_last_digit(int n)
 {
-	if (n <= 0)
-	{
-		n = (n * -1);
-	}
 	n = (n % 10);
 
-	_putchar('0' + n);
+	if (n < 0)
+	{
+		n = -n;
+	}
+
+	_putchar(n + '0');
 	return (n);
 }
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,13 @@
+/**
+ * _islower  - Short description, single line
+ * @c: contians value to be compared
+ * Return: Always returns zero
+ */
+
+void jack_bauer(void)
+{
+	int i;
+
+	for (int i )
+}
+
