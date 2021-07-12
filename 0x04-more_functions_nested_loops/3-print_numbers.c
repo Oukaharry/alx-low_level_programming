dfd123e0e22de810cc5354feeba1ab5ce434a2e8
OukaHarry <harryodhiambo17@gmail.com>
2021-07-12 05:41:31 -0700

    Print numbers edit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *print_numbers  - prints the numbers, from 0 to 9, followed by a new line.
+ *Return
+ */
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		_putchar(i);
+
+		i++;
+	}
+
+	_putchar('\n');
+}
