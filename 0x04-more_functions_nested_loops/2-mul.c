a761464f8b076e84ddae0c20b38c7b6cccf914dd
OukaHarry <harryodhiambo17@gmail.com>
2021-07-12 05:02:37 -0700

    add file mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *mul - function that checks for a digit
+ *Return: a * b on true
+ *@a: parameter
+ *@b: parameter
+ */
+
+int mul(int a, int b)
+{
+	int res = a * b;
+
+	return (res);
+}
