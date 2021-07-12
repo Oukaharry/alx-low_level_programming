c3180ceae04c079667b0ca44394d34c6167ce6a2
OukaHarry <harryodhiambo17@gmail.com>
2021-07-12 02:15:56 -0700

    edit on file isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _isupper  - Short description, single line
+ * @c: contians value to be compared
+ * Return: value of isupper
+ */
+
+int __isupper(int c)
+{
+	int isupper = 0;
+	char ch  = c;
+
+	if (ch > 64 && ch <= 90)
+	{
+		isupper = 1;
+	}
+
+	else
+	{
+		isupper = 0;
+	}
+
+	return (isupper);
+}
